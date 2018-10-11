446dc5531c92232cc2a0873bbb6d3c93716a8349
takumanaruke <leon32taku@gmail.com>
2018-10-12 00:17:38 +0900

    Add get request sample

--- app/controllers/rakuten_hotels_controller.rb
+++ app/controllers/rakuten_hotels_controller.rb
@@ -1,6 +1,15 @@
 class RakutenHotelsController < ApplicationController
 
+  require 'net/http'
+  require 'json'
+
   def search
-    @hotels = https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?applicationId=1089373237716979823&format=json&largeClassCode=japan&middleClassCode=akita&smallClassCode=tazawa&checkinDate=2018-10-12&checkoutDate=2018-10-14&adultNum=2
+    uri = URI('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?applicationId=1089373237716979823&format=json&largeClassCode=japan&middleClassCode=akita&smallClassCode=tazawa&checkinDate=2018-10-12&checkoutDate=2018-10-14&adultNum=2')
+    resp = Net::HTTP.get_response(uri)
+    #http.use_ssl = true
+    #req = Net::HTTP::Get.new(uri.path)
+    #resp = http.request(req)
+    result = JSON.parse(resp.body)
+    @hotels = result["hotels"]
   end
 end
