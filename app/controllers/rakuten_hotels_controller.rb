9246761913098b65343bad7885691b847506defc
takumanaruke <leon32taku@gmail.com>
2018-10-12 17:27:10 +0900

    create rakuten view

--- app/controllers/rakuten_hotels_controller.rb
+++ app/controllers/rakuten_hotels_controller.rb
@@ -4,13 +4,17 @@ class RakutenHotelsController < ApplicationController
   require 'json'
 
   def search
-    uri = URI('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?applicationId=1089373237716979823&format=json&largeClassCode=japan&middleClassCode=akita&smallClassCode=tazawa&checkinDate=2018-10-12&checkoutDate=2018-10-14&adultNum=2')
+    uri = URI('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?applicationId=1089373237716979823&format=json&largeClassCode=japan&middleClassCode=akita&smallClassCode=tazawa&checkinDate=2018-10-15&checkoutDate=2018-10-16&adultNum=2')
     resp = Net::HTTP.get_response(uri)
     #http.use_ssl = true
     #req = Net::HTTP::Get.new(uri.path)
     #resp = http.request(req)
     result = JSON.parse(resp.body)
     @hotels = result["hotels"]
+  end
+
+  def show
+    @hotel = Hotel.find(@hotel)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ resources :users
 get 'login' => 'user_sessions#new', :as => :login
 post 'logout' => 'user_sessions#destroy', :as => :logout
 get 'rakuten_search' => 'rakuten_hotels#search'
+get 'rakuten_show' => 'rakuten_hotels#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
