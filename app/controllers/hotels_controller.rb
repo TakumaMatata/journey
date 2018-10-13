b388ecd18c7a0a59d9d8a2086f29f08886f41a4e
takumanaruke <leon32taku@gmail.com>
2018-10-13 20:51:20 +0900

    serch by lang and long

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -7,18 +7,26 @@ class HotelsController < ApplicationController
 
     if params.has_key?(:num_of_ppl)
 
+      location = params[:location]
+      area = Geocoder.search(location)
+      area.first.coordinates
+
+      latitude_raw = area.first.coordinates[0]
+      longitude_raw = area.first.coordinates[1]
+
+      latitude = (latitude_raw * 3600).round(2)
+      longitude = (longitude_raw * 3600).round(2)
+
       num_of_ppl = params[:num_of_ppl]
       check_in_date = params[:check_in]
-      check_out_date = '2018-10-18'
-      # latitude = params[:latitude]
-      # longitude = params[:longitude]
+      check_out_date = params[:check_out]
 
       uri = URI('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426')
       data = {
         :applicationId => '1089373237716979823',
         :format => 'json',
-        :latitude => '128440.51',
-        :longitude => '503172.21',
+        :latitude => latitude,
+        :longitude => longitude,
         :searchRadius => '1',
         :checkinDate => check_in_date,
         :checkoutDate => check_out_date,
@@ -28,16 +36,12 @@ class HotelsController < ApplicationController
 
       uri.query = URI.encode_www_form(data)
 
-      puts uri
-
       resp = Net::HTTP.get_response(uri)
       #http.use_ssl = true
       #req = Net::HTTP::Get.new(uri.path)
       #resp = http.request(req)
       result = JSON.parse(resp.body)
-
-      puts result
-
+puts result
       @hotels = result["hotels"]
 
     else
@@ -69,6 +73,6 @@ class HotelsController < ApplicationController
 
   private
   def hotel_params
-    params.require(:hotel).permit(:location, :num_of_ppl, :check_in)
+    params.require(:hotel).permit(:location, :num_of_ppl, :check_in, :check_out)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_030258) do
+ActiveRecord::Schema.define(version: 2018_10_13_101553) do
 
   create_table "hotels", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
